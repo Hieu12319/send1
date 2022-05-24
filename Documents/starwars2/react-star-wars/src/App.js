c00e21d6f9816de1d06dab56d813a68e44273b85
Hieu12319 <97079165+Hieu12319@users.noreply.github.com>
2022-05-24 14:51:26 -0700

    got ship names to render onto the page

--- Documents/starwars2/react-star-wars/src/App.js
+++ Documents/starwars2/react-star-wars/src/App.js
@@ -4,25 +4,33 @@ import './App.css';
 import {useState, useEffect } from "react";
 
 
+// function App() {
+//   const [starships, setStarships] = useState(null);
+//   const getStarships = async () => {
+//     const response = await fetch(`http https://swapi.dev/api/starships`);
+//     const data = await response.json();
+//     setStarships(data);
+//     console.log(data.results);
+//   }
 function App() {
-  const [starships, setStarships] = useState(null);
-  const getStarships = async () => {
-    const response = await fetch(`http https://swapi.dev/api/starships`);
-    const data = await response.json();
-    setStarships(data);
-    console.log(data.results);
+  
+  const [starships, setStarships] = useState(null)
+  
+  const getShip = async () =>{
+    const response = await fetch("https://swapi.dev/api/starships/")
+
+    const data = await response.json()
+    setStarships(data)
   }
 
-
-useEffect(() => {getStarships()},[]);
+useEffect(() => {getShip()},[]);
 
 return(
   <div className = "App">
-    <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/300px-Star_Wars_Logo.svg.png' alt='Star Wars Logo'/>
-  
-    <section className="cards">
-      {starships && starships?.results?.map(starships => (
-        <Starships name={starships.name} />
+    <img src='https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/maxresdefault-13.jpg?auto=format&q=60&fit=max&w=930' alt='Star Wars Logo'/>
+    <section className='cards'>
+      {starships && starships.results.map(starshipsElement => (
+        <Starships name={starshipsElement.name} />
       ))}
   </section>
   </div> 
